6a02edfd89c8a3a93489adacf1928604e5a5f0ea
Noam Z <nzoam93@gmail.com>
2023-03-10 18:04:22 -0800

    added main pic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import Header from "./components/Navigation"
+import Header from "./components/Navigation";
+import MainPic from "./components/MainPic"
+
+
 function App() {
   return (
-    <Header/>
+    <>
+      <Header/>
+      <MainPic/>
+    </>
   );
 }
 
---
+++ src/components/MainPic/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./MainPic.css"
+
+const mainPic = () => {
+  return (
+    <div id='main-pic'>
+      <p id='main-pic-first-line'>REFLEXOLO-CHI</p>
+      <p id='main-pic-second-line'>MASSAGING SOLES ... HEALING SOULS</p>
+    </div>
+  )
+}
+
+export default mainPic
